ce504fbee2cb589938fe8828c50927c24255c044
barkatzx <barkatullah.zx@gmail.com>
2023-05-22 17:41:40 +0600

    Search Bar Added In All Toys & 20 Limit Added

--- src/Components/AllToys/AllToys.jsx
+++ src/Components/AllToys/AllToys.jsx
@@ -3,23 +3,42 @@ import { useLoaderData } from 'react-router-dom';
 import ToysCard from '../ToysCard/ToysCard';
 
 const AllToys = () => {
-    const loadedToys = useLoaderData();
-    const [ toys, setToys ] = useState(loadedToys);
-    return (
-        <div className='bg-slate-100 rounded-2xl shadow-2xl'>
-            <h1 className='text-3xl text-center font-bold'>All Toys: {toys.length}</h1>
-            <div className=''>
-                {
-                    toys.map(toy => <ToysCard key={toy._id}
-                        toy={toy}
-                        toys={toys}
-                        setToys={setToys}></ToysCard>
-                        )
-                }
-            </div>
-            
-        </div>
-    );
+  const loadedToys = useLoaderData();
+  const [toys, setToys] = useState(loadedToys);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredToys = toys.filter(toy =>
+    toy.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+ 
+  const limitedToys = filteredToys.slice(0, 20);
+
+  return (
+    <div className="bg-slate-100 rounded-2xl shadow-2xl p-8">
+      <h1 className="text-3xl text-center font-bold mb-4">
+        All Toys: {toys.length}
+      </h1>
+      <input
+          type="text"
+          placeholder="Search by toy name"
+          value={searchTerm}
+          className='justify-center-center input w-full max-w-xs p-4 mb-4'
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+      <div className="">
+        
+        {limitedToys.map(toy => (
+          <ToysCard
+            key={toy._id}
+            toy={toy}
+            toys={toys}
+            setToys={setToys}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllToys;
--- src/Components/ToysCard/ToysCard.jsx
+++ src/Components/ToysCard/ToysCard.jsx
@@ -2,24 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const ToysCard = ({ toy, toys, setToys }) => {
-    const {_id, name, quantity, seller, price, sub_category, photo } = toy;
-    return (
-        <div className='container mx-auto'>
-              <div className="card lg:card-side bg-base-100 shadow-2xl">
-  <figure><img src={photo} alt="Photo" className="w-24 h-auto md:w-32 lg:w-40 object-cover"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">{name}</h2>
-    <p>{seller}</p>
-    <p>{quantity}</p>
-    <p>{sub_category}</p>
-    <p>{price}</p>
-    <div className="card-actions">
-      <button className="btn btn-primary">Details</button>
-    </div>
-  </div>
-</div>
+  const { _id, name, quantity, seller, price, sub_category, photo } = toy;
+
+  return (
+    <div className="container mx-auto flex justify-center">
+      <div className="card lg:card-side bg-base-100 shadow-2xl mb-1 w-full md:w-1/2 lg:w-1/3 p-4">
+        <figure>
+          <img
+            src={photo}
+            alt="Photo"
+            className="w-full h-64 object-cover"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-2xl">{name}</h2>
+          <p className="text-lg">Seller Name: {seller}</p>
+          <p>Available Quantity: {quantity}</p>
+          <p>Sub Category: {sub_category}</p>
+          <p>Price: {price}</p>
+          <div className="card-actions">
+            <button className="btn btn-primary">Details</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ToysCard;
