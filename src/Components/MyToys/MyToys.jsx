08d803e767e4c1d56f4b3e6223e2403c3797b96d
barkatzx <barkatullah.zx@gmail.com>
2023-05-23 00:48:35 +0600

    My Toys Added

--- src/Components/MyToys/MyToys.jsx
+++ src/Components/MyToys/MyToys.jsx
@@ -10,7 +10,7 @@ const MyToys = () => {
         // Make an API request to retrieve the user's toys data
         const response = await fetch('http://localhost:5000/toys', {
           headers: {
-            Authorization: 'Bearer <your_access_token>', // Include the user's access token for authentication
+            Authorization: 'Bearer {accessToken}',
           },
         });
 
@@ -38,12 +38,30 @@ const MyToys = () => {
 
   return (
     <div>
-      {/* Display the fetched toys data */}
+        <div className='text-3xl font-bold text-center mb-2'>Your Product List</div>
       {toysData.map((toy) => (
-        <div key={toy.id}>
-          <h2>Title: {toy.title}</h2>
+        <div key={toy._id}>
+          <div className="container mx-auto flex justify-center">
+      <div className="card lg:card-side bg-base-100 shadow-2xl mb-1 w-full p-8">
+        <figure>
+          <img
+            src={toy.photo}
+            alt="Photo"
+            className="w-full object-cover"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-2xl">{toy.name}</h2>
+          <h2 className="card-title text-lg">Seller Name: {toy.seller}</h2>
+          <p>Email: {toy.email}</p>
+          <p>Available Quantity: {toy.quantity}</p>
+          <p>Sub Category: {toy.sub_category}</p>
+          <p>Rating: {toy.rating}</p>
           <p>Details: {toy.details}</p>
-          {/* ...other toy information */}
+          <p>Price: {toy.price}</p>
+        </div>
+      </div>
+    </div>
         </div>
       ))}
     </div>
